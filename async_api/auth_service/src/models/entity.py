9f8efa0a988a72323e1d1e14567c54ac553a4fda
Vasiliy Dronov <vasiliyasp@gmail.com>
2024-06-11 12:07:39 +0800

    changed models: deleted constraints in primary key as they are not needed, changed Column to mapped_column, deleted __init__ as sqlalchemy creates default one

--- async_api/auth_service/src/models/entity.py
+++ async_api/auth_service/src/models/entity.py
@@ -1,10 +1,9 @@
 import uuid
-from datetime import datetime
+import time
 
-from typing import Annotated
-from sqlalchemy import Boolean, Column, DateTime, String, ForeignKey
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import Boolean, DateTime, String, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
-from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy.orm import relationship, DeclarativeBase
 from db.postgres import Base
 
@@ -16,37 +15,28 @@ class Base(DeclarativeBase):
 class UserRole(Base):
     __tablename__ = 'users_roles'
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    user_id= Column(UUID(as_uuid=True), ForeignKey("users.id"))
-    role_id = Column(UUID(as_uuid=True), ForeignKey("roles.id"))
-    updated_at = Column(DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
+    id = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id= mapped_column(UUID(as_uuid=True), ForeignKey("users.id"))
+    role_id = mapped_column(UUID(as_uuid=True), ForeignKey("roles.id"))
+    updated_at = mapped_column(DateTime, default=time.time(), onupdate=time.time())
 
 
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    login = Column(String(255), unique=True, nullable=False)
-    password = Column(String(255), nullable=False)
-    first_name = Column(String(50), nullable=False)
-    last_name = Column(String(50), nullable=False)
-    email = Column(String(255), nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-    deleted_at = Column(Boolean, default=True)
+    id = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    login = mapped_column(String(255), unique=True, nullable=False)
+    password = mapped_column(String(255), nullable=False)
+    first_name = mapped_column(String(50), nullable=False)
+    last_name = mapped_column(String(50), nullable=False)
+    email = mapped_column(String(255), nullable=False)
+    created_at = mapped_column(DateTime, default=time.time)
+    updated_at = mapped_column(DateTime, default=time.time, onupdate=time.time)
+    deleted_at = mapped_column(Boolean, default=True) #TODO: rename to is_deleted
     # is_superadmin = Column(Boolean, default=False)
     roles = relationship("Role", secondary='users_roles', back_populates='users', lazy='selectin')
 
     # history = relationship("History", secondary=)
-
-    def __init__(self, login: str, password: str, first_name: str, last_name: str) -> None:
-        self.login = login
-        self.password = generate_password_hash(password)
-        self.first_name = first_name
-        self.last_name = last_name
-
-    def check_password(self, password: str) -> bool:
-        return check_password_hash(self.password, password)
 
 
     def __repr__(self) -> str:
@@ -56,16 +46,12 @@ class User(Base):
 class Role(Base):
     __tablename__ = 'roles'
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    title = Column(String(255), unique=True, nullable=False)
-    description = Column(String(255), nullable=True)
-    created_at = Column(DateTime, default=datetime.utcnow())
-    updated_at = Column(DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
+    id = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    title = mapped_column(String(255), unique=True, nullable=False)
+    description = mapped_column(String(255), nullable=True)
+    created_at = mapped_column(DateTime, default=time.time)
+    updated_at = mapped_column(DateTime, default=time.time, onupdate=time.time)
     users = relationship("User", secondary='users_roles', back_populates='roles', lazy='selectin')
-
-    def __init__(self, title: str, description: str = None) -> None:
-        self.title = title
-        self.description = description
 
     def __repr__(self):
         return f'<Role {self.title}>'
@@ -73,11 +59,11 @@ class Role(Base):
 class UserHistory(Base):
     __tablename__ = 'user_history'
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
-    occured_at = Column(DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
-    action = Column(String(255), nullable=False)
-    fingreprint = Column(String(255), nullable=False)
+    id = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = mapped_column(UUID(as_uuid=True), ForeignKey("users.id"))
+    occured_at = mapped_column(DateTime, default=time.time, onupdate=time.time)
+    action = mapped_column(String(255), nullable=False)
+    fingreprint = mapped_column(String(255), nullable=False)
 
-    def __init__(self):
-        pass
+    def __repr__(self):
+        return f'<UserHistory {self.user_id} - {self.action}>'
