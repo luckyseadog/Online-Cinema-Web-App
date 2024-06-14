c452b3e7def4ac23da85b5cd5cbf61cef8c61977
Aleksey Skovorodnikov <alexskovorodnikov@yandex.ru>
2024-06-14 22:05:38 +0300

    Add fixture for create database

--- async_api/auth_service/src/tests/conftest.py
+++ async_api/auth_service/src/tests/conftest.py
@@ -1,38 +1,15 @@
 import asyncio
+
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 import pytest_asyncio
 from models.entity import Base
 import httpx
 from main import app
-
-# DATABASE_URL_TEST = f'postgresql+asyncpg:{
-# DB_USER_TEST}:{DB_PASSWORD_TEST}@{DB_HOST_TEST}
-# :{DB_PORT_TEST}/{DB_NAME_TEST}'
-DSN = 'postgresql+asyncpg://app:123qwe@127.0.0.1:5432/auth_test'
-engine_test = create_async_engine(DSN, echo=True, future=True)
-async_session = sessionmaker(engine_test, class_=AsyncSession, expire_on_commit=False)
-
-
-async def create_database() -> None:
-    async with engine_test.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-
-async def purge_database() -> None:
-    async with engine_test.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-
-
-async def get_session() -> AsyncSession:
-    async with async_session() as session:
-        yield session
-
-@pytest_asyncio.fixture(scope='session')
-async def prepare_database():
-    await create_database()
-    yield
-    await purge_database()
+from core.config import settings
+import json
+import os
+from models.entity import RoleModel, UserModel, UserRoleModel, UserHistoryModel
 
 
 @pytest_asyncio.fixture(scope='session')
@@ -42,14 +19,59 @@ def event_loop():
     loop.close()
 
 
-# @pytest_asyncio.fixture(scope='session')
-# async def client():
-#     async with AsyncClient(app=app, base_url="http://test") as client:
-#         yield client
-
-
-@pytest_asyncio.fixture
+@pytest_asyncio.fixture(scope='session')
 async def client():
     async with httpx.AsyncClient(app=app, base_url='http://test') as client:
-        print('Client is ready')
         yield client
+
+
+@pytest_asyncio.fixture(scope='session')
+async def prepare_database():
+    DSN = (
+        f'postgresql+asyncpg://{settings.pg_user}:{settings.pg_password}'
+        f'@{settings.pg_host}:{settings.pg_port}/{settings.pg_name}'
+    )
+
+    engine = create_async_engine(DSN, echo=True, future=True)
+    async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    async with async_session() as session:
+        with open(f'{os.path.dirname(os.path.realpath(__file__))}/functionals/testdata/data/roles.json') as file:
+            for item in json.load(file):
+                role = RoleModel(title=item['title'], description=item['description'])
+                session.add(role)
+            # await session.commit()
+
+        with open(f'{os.path.dirname(os.path.realpath(__file__))}/functionals/testdata/data/users.json') as file:
+            for item in json.load(file):
+                user = UserModel(
+                    id=item['id'],
+                    login=item['login'],
+                    password=item['password'],
+                    email=item['email'],
+                    first_name=item['first_name'],
+                    last_name=item['last_name'],
+                )
+                print(item['roles'])
+                for role in item['roles']:
+                    user_role = UserRoleModel(user_id=item['id'], role_id=role['id'])
+                    session.add(user_role)
+                session.add(user)
+
+                for history in item['history']:
+                    user_history = UserHistoryModel(
+                        user_id=item['id'],
+                        occured_at=history['occured_at'],
+                        action=history['action'],
+                        fingreprint=history['fingerprint'],
+                    )
+                    session.add(user_history)
+        await session.commit()
+
+    yield
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
--- async_api/auth_service/src/tests/functionals/src/test_roles.py
+++ async_api/auth_service/src/tests/functionals/src/test_roles.py
@@ -1,11 +1,8 @@
 import pytest
-from httpx import AsyncClient
 
 
 @pytest.mark.asyncio
-async def test_create_database(client: AsyncClient, prepare_database):
+async def test_get_roles(client, prepare_database):
     response = await client.get('/api/v1/auth/admin/roles')
-    print('=' * 100)
     print(response.json())
-    print('=' * 100)
     assert response.status_code == 200
