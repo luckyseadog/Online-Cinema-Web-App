3ca433f8479c5a35c83ba0917ab5606b44d6c1eb
Aleksey Skovorodnikov <alexskovorodnikov@yandex.ru>
2024-06-27 16:09:01 +0300

    refactor api admin

--- async_api/auth_service/src/api/v1/admin.py
+++ async_api/auth_service/src/api/v1/admin.py
@@ -1,14 +1,15 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Header, HTTPException, status
+from fastapi import APIRouter, Depends, status
 
 from schemas.entity import User
-from schemas.entity_schemas import AccessTokenData, UpdateUserRole
+from schemas.entity_schemas import UpdateUserRole
 from services.validation import check_admin_or_super_admin_role_from_access_token
-from services.user_service import UserService, get_user_service
 from services.admin_service import AdminService, get_admin_service
 
-router = APIRouter()
+router = APIRouter(
+    dependencies=[Depends(check_admin_or_super_admin_role_from_access_token)],
+)
 
 
 @router.post(
@@ -26,16 +27,8 @@ router = APIRouter()
 )
 async def assign_role(
     user_role: UpdateUserRole,
-    payload: Annotated[AccessTokenData, Depends(check_admin_or_super_admin_role_from_access_token)],
-    user_service: Annotated[UserService, Depends(get_user_service)],
     admin_service: Annotated[AdminService, Depends(get_admin_service)],
 ) -> User:
-
-    if await user_service.check_deleted(payload.sub):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail='User was deleted',
-        )
     updated_user = await admin_service.assign_user_role(str(user_role.user_id), str(user_role.role_id))
     return updated_user
 
@@ -49,19 +42,9 @@ async def assign_role(
 )
 async def revoke_role(
     user_role: UpdateUserRole,
-    payload: Annotated[AccessTokenData, Depends(check_admin_or_super_admin_role_from_access_token)],
     admin_service: Annotated[AdminService, Depends(get_admin_service)],
-    user_service: Annotated[UserService, Depends(get_user_service)],
-    user_agent: Annotated[str | None, Header()] = None,
 ) -> User:
-
-    if await user_service.check_deleted(payload.sub):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail='User was deleted',
-        )
-    updated_user = await admin_service.revoke_user_role(str(user_role.user_id), str(user_role.role_id))
-    return updated_user
+    return await admin_service.revoke_user_role(str(user_role.user_id), str(user_role.role_id))
 
 
 @router.post(
@@ -72,18 +55,8 @@ async def revoke_role(
 )
 async def check_role(
     user_role: UpdateUserRole,
-    payload: Annotated[AccessTokenData, Depends(check_admin_or_super_admin_role_from_access_token)],
     admin_service: Annotated[AdminService, Depends(get_admin_service)],
-    # ?payload=Annotated[ValidationService, Depends(get_validation_service)],
-    user_service: Annotated[UserService, Depends(get_user_service)],
-    user_agent: Annotated[str | None, Header()] = None,
+    # user_agent: Annotated[str | None, Header()] = None,
 ):
-
-    if await user_service.check_deleted(payload.sub):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail='User was deleted',
-        )
-    # res = await admin_service.check_user_role(str(user_role.user_id), str(user_role.))
     res = await admin_service.check_user_role(str(user_role.user_id), str(user_role.role_id))
     return {'result': 'YES' if res else 'NO'}
