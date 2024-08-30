d71fce092e07f70fff94351c2a18cd8a27276ff9
Plotnikov Petr <petr77793@gmail.com>
2024-08-31 02:31:24 +0700

    view change_of_right

--- auth-service/src/api/v1/models/access_control.py
+++ auth-service/src/api/v1/models/access_control.py
@@ -1,7 +1,27 @@
+from uuid import UUID
+
 from pydantic import BaseModel
 from pydantic.fields import Field
 
 
-class RightModel(BaseModel):
+class CreateRightModel(BaseModel):
     name: str = Field(description="Название права", title="Название")
-    description: str = Field(description="Описание права", title="Описание")
+    description: str | None = Field(default=None, description="Описание права", title="Описание")
+
+
+class DeleteRightModel(BaseModel):
+    id: UUID | None = Field(default=None, description="Идентификатор права", title="Идентификатор")
+    name: str | None = Field(default=None, description="Название права", title="Название")
+
+
+class ChangeRightModel(BaseModel):
+    id: UUID | None = Field(default=None, description="Идентификатор права", title="Идентификатор")
+    current_name: str | None = Field(default=None, description="Текущее название права", title="Название")
+    name: str | None = Field(default=None, description="Новое название права", title="Новое название")
+    description: str | None = Field(default=None, description="Новое описание права", title="Новое описание")
+
+
+class RightModel(BaseModel):
+    id: UUID
+    name: str
+    description: str | None
