9737bb4154e2d96543931852de8006ee6034ce6f
Plotnikov Petr <petr77793@gmail.com>
2024-10-23 01:57:16 +0700

    ruff fix

--- etl_kafka_to_mongo/db/mongo_db.py
+++ etl_kafka_to_mongo/db/mongo_db.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+
 from pymongo import MongoClient
 from pymongo.synchronous.database import Database
 
@@ -7,5 +8,5 @@ from core.config import configs
 
 @lru_cache
 def get_db_ugc() -> Database:
-    client = MongoClient(configs.mongo_host_ugc, configs.mongo_port_ugc, uuidRepresentation='standard')
+    client = MongoClient(configs.mongo_host_ugc, configs.mongo_port_ugc, uuidRepresentation="standard")
     return client[configs.mongo_name]
--- etl_kafka_to_mongo/services/ugc_storage_service.py
+++ etl_kafka_to_mongo/services/ugc_storage_service.py
@@ -1,12 +1,12 @@
 from functools import lru_cache
 from uuid import UUID, uuid4
 
+from pymongo.synchronous.database import Database
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import select
-from pymongo.synchronous.database import Database
 
-from db.postgres_db import get_session_admin
 from db.mongo_db import get_db_ugc
+from db.postgres_db import get_session_admin
 from models.alchemy_model import FilmWork
 
 
@@ -20,10 +20,10 @@ class UGCStorageService:
         self.session_admin = session_admin
 
     def _add_rating(self, user_id: UUID, film_id: UUID, rating: float) -> None:
-        if existing_document := self.mongo_db.ratings.find_one({'user_id': user_id, 'film_id': film_id}):
-            self.mongo_db.ratings.update_one({'_id': existing_document['_id']}, {'$set': {'rating': rating}})
+        if existing_document := self.mongo_db.ratings.find_one({"user_id": user_id, "film_id": film_id}):
+            self.mongo_db.ratings.update_one({"_id": existing_document["_id"]}, {"$set": {"rating": rating}})
         else:
-            self.mongo_db.ratings.insert_one({'_id': uuid4(), 'user_id': user_id, 'film_id': film_id, 'rating': rating})
+            self.mongo_db.ratings.insert_one({"_id": uuid4(), "user_id": user_id, "film_id": film_id, "rating": rating})
 
     def _update_film_rating(self, film_id: UUID, rating: float) -> None:
         stmt = select(FilmWork).where(FilmWork.id == film_id)
@@ -36,18 +36,18 @@ class UGCStorageService:
         self._update_film_rating(film_id, rating)
 
     def add_review(self, user_id: UUID, film_id: UUID, review: str) -> None:
-        if existing_document := self.mongo_db.reviews.find_one({'user_id': user_id, 'film_id': film_id}):
-            self.mongo_db.reviews.update_one({'_id': existing_document['_id']}, {'$set': {'review': review}})
+        if existing_document := self.mongo_db.reviews.find_one({"user_id": user_id, "film_id": film_id}):
+            self.mongo_db.reviews.update_one({"_id": existing_document["_id"]}, {"$set": {"review": review}})
         else:
-            self.mongo_db.reviews.insert_one({'_id': uuid4(), 'user_id': user_id, 'film_id': film_id, 'review': review})
+            self.mongo_db.reviews.insert_one({"_id": uuid4(), "user_id": user_id, "film_id": film_id, "review": review})
 
     def add_favourite(self, user_id: UUID, film_id: UUID) -> None:
-        if not self.mongo_db.favourites.find_one({'user_id': user_id, 'film_id': film_id}):
-            self.mongo_db.favourites.insert_one({'_id': uuid4(), 'user_id': user_id, 'film_id': film_id})
+        if not self.mongo_db.favourites.find_one({"user_id": user_id, "film_id": film_id}):
+            self.mongo_db.favourites.insert_one({"_id": uuid4(), "user_id": user_id, "film_id": film_id})
 
     def del_favourite(self, user_id: UUID, film_id: UUID) -> None:
-        if self.mongo_db.favourites.find_one({'user_id': user_id, 'film_id': film_id}):
-            self.mongo_db.favourites.delete_one({'user_id': user_id, 'film_id': film_id})
+        if self.mongo_db.favourites.find_one({"user_id": user_id, "film_id": film_id}):
+            self.mongo_db.favourites.delete_one({"user_id": user_id, "film_id": film_id})
 
 
 @lru_cache
